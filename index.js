13577ba688c3d6683998a4b9b369a4c2b4e0c84e
Alexandre Magedanz <alexandre.magedanz@gmail.com>
2025-01-17 19:14:26 +0000

    Divisão de responsabilidades

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
 const express = require('express')
-
+const { apiSomar, apiSubtrair, apiMultiplicar, apiDividir } = require('./api/operacoes')
 const app = express()
 const porta = 3000
 app.use(express.json())
 
-function somar(num1, num2) {
-    return num1 + num2
-}
+app.post('/somar', apiSomar)
 
-app.post('/somar', (req, res) =>{
-    const {num1 , num2} = req.body
-    const result =  somar(num1, num2)
-    res.status(200).send({result})
-})
+app.post('/sub', apiSubtrair)
 
-app.get('/subtrair', (req, res) =>{
-    const {num1 , num2} = req.query
-    res.send('A subtração é ' + (Number(num1) - Number(num2)))
-})
+app.post('/mult', apiMultiplicar)
 
-app.get('/multiplicar', (req, res) =>{
-    const {num1 , num2} = req.query
-    res.send('A multiplicação é ' + (Number(num1) * Number(num2)))
-})
-
-app.get('/dividir', (req, res) =>{
-    const {num1 , num2} = req.query
-    res.send('A multiplicação é ' + (Number(num1) / Number(num2)))
-})
-
-app.get('/parimpar', (req, res) =>{
-    const {num1} = req.query
-    let tipo = ''
-
-    if ((num1 % 2) == 0) {
-        tipo = 'Par'
-    } else {
-        tipo = 'Impar'
-    }
-    res.send('A numero é ' + tipo)
-})
+app.post('/div', apiDividir)
 
 app.listen(porta, () => {
     console.log('Servidor Rodando na porta ' + porta + '!')
